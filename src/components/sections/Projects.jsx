fa46c500a8cfd32c4155e53858bcc4dc24a7eba5
Radu <mihairadu8039@gmail.com>
2025-02-25 11:40:30 +0200

    Added a new project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -127,6 +127,47 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Youtube Bookmark</h3>
+              <p className="text-gray-400 mb-4">
+                The YouTube Video Bookmark Extension is a Chrome extension that
+                allows users to save and access specific timestamps in YouTube
+                videos quickly. Ideal for studying, research, or revisiting
+                favorite moments.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML", "Javascript", "CSS"].map((tech) => (
+                  <span
+                    key={tech}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/NicolaeRDU/YT-Bookmark-for-chome"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </RevealOnScroll>
